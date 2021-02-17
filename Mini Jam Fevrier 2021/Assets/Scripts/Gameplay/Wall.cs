64004d9be850212d2fd5849da94ae0fdada6c323
Allosius27 <yanis.queroix@orange.fr>
2021-02-17 22:25:49 +0100

    Mise a jour Projet Unity : 17/02/2021 : 22:25

---
+++ Mini Jam Fevrier 2021/Assets/Scripts/Gameplay/Wall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public float forceMult = 100.0f;
+    private Vector3 collisionDir = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D hit)
+    {
+
+        Entity typeCollision = hit.gameObject.GetComponent<Entity>();
+
+        if (typeCollision == null)
+        {
+            return;
+        }
+
+        if (typeCollision.Type == Entity.TypeObject.Player)
+        {
+            //Debug.Log(this.name + "Collides");
+            collisionDir = new Vector3(hit.transform.position.x - transform.position.x, hit.transform.position.y - transform.position.y, 0);
+            collisionDir = collisionDir.normalized;
+            hit.rigidbody.AddForce(collisionDir * forceMult);
+
+            
+        }
+    }
+}
