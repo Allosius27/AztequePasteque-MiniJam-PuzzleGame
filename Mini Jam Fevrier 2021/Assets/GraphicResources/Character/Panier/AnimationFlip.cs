cfccc4c69590b6e850227e7f562650643d5fdac9
Heleien <jeanne.rossat@lacatholille.fr>
2021-02-18 09:34:24 +0100

    no message

--- Mini Jam Fevrier 2021/Assets/GraphicResources/Character/Panier/AnimationFlip.cs
+++ Mini Jam Fevrier 2021/Assets/GraphicResources/Character/Panier/AnimationFlip.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class AnimationFlip : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator anim;
+    public GameObject TargetA;
+    public GameObject TargetB;
+    public GameObject Player;
+    //private Vector2 scaleChange;
 
+    //private void OnCollisionEnter(Collision2D collision)
+    //{
+    //    if (collision.gameObject)
+    //    {
+    //        Player.transform.localscale = new Vector2(-1, 0);
+
+    //    }
+    //}
     // Update is called once per frame
     void Update()
     {
